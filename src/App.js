a2f1d90cf4536aa562db2952e45f876aafafd48d
conradtrost <ct.conradtrost@gmail.com>
2021-07-09 14:13:35 -0500

    init files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="NoteTaker">
+        <Helmet>
+          <title>Note Builder</title>
+        </Helmet>
+
+        <Header />
+
+        <Router>
+          <Switch>
+              <Route exact path="/">
+                  <Home />
+              </Route>
+          </Switch>
+        </Router>
+
+        <Footer />
     </div>
   );
 }
